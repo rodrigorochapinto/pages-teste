a8f598a6219f59900d69eaa658094e109a4ccba1
Rodrigo Rocha <2rptecnologia@gmail.com>
2019-01-19 17:08:22 -0200

    Update pirata.js

--- js/pirata.js
+++ js/pirata.js
@@ -1,68 +1,66 @@
-/////////////////////////////////////////////////////////
-// Javascript made by http://peters1.dk/tools/snow.php //
-/////////////////////////////////////////////////////////
+// Rodrigo Rocha 19/01/2019
 
-// LEMBRE-SE: De mudar o caminho, onde snow.png é salvo
-snow_img = "https://rodrigorochapinto.github.io/pages-teste/imagens/pirata.png";
+// Caminho da Imagem
+pirata_img = "https://rodrigorochapinto.github.io/pages-teste/imagens/pirata.png";
 
-// EXTRA: Voce pode facilmente ajustar o número de flocos de neve que voce deseja em cada página...
-snow_no = 25;
+// Números de elementos caindo na tela
+pirata_no = 25;
 
 if (typeof(window.pageYOffset) == "number")
 {
-	snow_browser_width = window.innerWidth;
-	snow_browser_height = window.innerHeight;
+	pirata_browser_width = window.innerWidth;
+	pirata_browser_height = window.innerHeight;
 } 
 else if (document.body && (document.body.scrollLeft || document.body.scrollTop))
 {
-	snow_browser_width = document.body.offsetWidth;
-	snow_browser_height = document.body.offsetHeight;
+	pirata_browser_width = document.body.offsetWidth;
+	pirata_browser_height = document.body.offsetHeight;
 }
 else if (document.documentElement && (document.documentElement.scrollLeft || document.documentElement.scrollTop))
 {
-	snow_browser_width = document.documentElement.offsetWidth;
-	snow_browser_height = document.documentElement.offsetHeight;
+	pirata_browser_width = document.documentElement.offsetWidth;
+	pirata_browser_height = document.documentElement.offsetHeight;
 }
 else
 {
-	snow_browser_width = 500;
-	snow_browser_height = 500;	
+	pirata_browser_width = 500;
+	pirata_browser_height = 500;	
 }
 
-snow_dx = [];
-snow_xp = [];
-snow_yp = [];
-snow_am = [];
-snow_stx = [];
-snow_sty = [];
+pirata_dx = [];
+pirata_xp = [];
+pirata_yp = [];
+pirata_am = [];
+pirata_stx = [];
+pirata_sty = [];
 
-for (i = 0; i < snow_no; i++) 
+for (i = 0; i < pirata_no; i++) 
 { 
-	snow_dx[i] = 0; 
-	snow_xp[i] = Math.random()*(snow_browser_width-50);
-	snow_yp[i] = Math.random()*snow_browser_height;
-	snow_am[i] = Math.random()*20; 
-	snow_stx[i] = 0.02 + Math.random()/10;
-	snow_sty[i] = 0.7 + Math.random();
-	if (i > 0) document.write("<\div id=\"snow_flake"+ i +"\" style=\"position:absolute;z-index:"+i+"\"><\img src=\""+snow_img+"\" border=\"0\"><\/div>"); else document.write("<\div id=\"snow_flake0\" style=\"position:absolute;z-index:0\"><a href=\"http://peters1.dk/tools/snow.php\" target=\"_blank\"><\img src=\""+snow_img+"\" border=\"0\"></a><\/div>");
+	pirata_dx[i] = 0; 
+	pirata_xp[i] = Math.random()*(pirata_browser_width-50);
+	pirata_yp[i] = Math.random()*pirata_browser_height;
+	pirata_am[i] = Math.random()*20; 
+	pirata_stx[i] = 0.02 + Math.random()/10;
+	pirata_sty[i] = 0.7 + Math.random();
+	if (i > 0) document.write("<\div id=\"pirata_flake"+ i +"\" style=\"position:absolute;z-index:"+i+"\"><\img src=\""+pirata_img+"\" border=\"0\"><\/div>"); else document.write("<\div id=\"pirata_flake0\" style=\"position:absolute;z-index:0\"><a href=\"http://peters1.dk/tools/pirata.php\" target=\"_blank\"><\img src=\""+pirata_img+"\" border=\"0\"></a><\/div>");
 }
 
-function SnowStart() 
+function PirataStart() 
 { 
-	for (i = 0; i < snow_no; i++) 
+	for (i = 0; i < pirata_no; i++) 
 	{ 
-		snow_yp[i] += snow_sty[i];
-		if (snow_yp[i] > snow_browser_height-50) 
+		pirata_yp[i] += pirata_sty[i];
+		if (pirata_yp[i] > pirata_browser_height-50) 
 		{
-			snow_xp[i] = Math.random()*(snow_browser_width-snow_am[i]-30);
-			snow_yp[i] = 0;
-			snow_stx[i] = 0.02 + Math.random()/10;
-			snow_sty[i] = 0.7 + Math.random();
+			pirata_xp[i] = Math.random()*(pirata_browser_width-pirata_am[i]-30);
+			pirata_yp[i] = 0;
+			pirata_stx[i] = 0.02 + Math.random()/10;
+			pirata_sty[i] = 0.7 + Math.random();
 		}
-		snow_dx[i] += snow_stx[i];
-		document.getElementById("snow_flake"+i).style.top=snow_yp[i]+"px";
-		document.getElementById("snow_flake"+i).style.left=snow_xp[i] + snow_am[i]*Math.sin(snow_dx[i])+"px";
+		pirata_dx[i] += pirata_stx[i];
+		document.getElementById("pirata_flake"+i).style.top=pirata_yp[i]+"px";
+		document.getElementById("pirata_flake"+i).style.left=pirata_xp[i] + pirata_am[i]*Math.sin(pirata_dx[i])+"px";
 	}
-	snow_time = setTimeout("SnowStart()", 10);
+	pirata_time = setTimeout("PirataStart()", 10);
 }
-SnowStart()
+PirataStart()
