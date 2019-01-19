2e166f6a62f07c087cf853e41c43f805e248b178
Rodrigo Rocha <2rptecnologia@gmail.com>
2019-01-19 17:01:40 -0200

    Update coracao_triplo.js

--- js/coracao_triplo.js
+++ js/coracao_triplo.js
@@ -1,68 +1,66 @@
-/////////////////////////////////////////////////////////
-// Javascript made by http://peters1.dk/tools/snow.php //
-/////////////////////////////////////////////////////////
+// Rodrigo Rocha 19/01/2019
 
-// LEMBRE-SE: De mudar o caminho, onde snow.png é salvo
-snow_img = "https://rodrigorochapinto.github.io/pages-teste/imagens/coracao_triplo.png";
+// Caminho da Imagem
+coracao_triplo_img = "https://rodrigorochapinto.github.io/pages-teste/imagens/coracao_triplo.png";
 
-// EXTRA: Voce pode facilmente ajustar o número de flocos de neve que voce deseja em cada página...
-snow_no = 25;
+// Números de elementos caindo na tela
+coracao_triplo_no = 25;
 
 if (typeof(window.pageYOffset) == "number")
 {
-	snow_browser_width = window.innerWidth;
-	snow_browser_height = window.innerHeight;
+	coracao_triplo_browser_width = window.innerWidth;
+	coracao_triplo_browser_height = window.innerHeight;
 } 
 else if (document.body && (document.body.scrollLeft || document.body.scrollTop))
 {
-	snow_browser_width = document.body.offsetWidth;
-	snow_browser_height = document.body.offsetHeight;
+	coracao_triplo_browser_width = document.body.offsetWidth;
+	coracao_triplo_browser_height = document.body.offsetHeight;
 }
 else if (document.documentElement && (document.documentElement.scrollLeft || document.documentElement.scrollTop))
 {
-	snow_browser_width = document.documentElement.offsetWidth;
-	snow_browser_height = document.documentElement.offsetHeight;
+	coracao_triplo_browser_width = document.documentElement.offsetWidth;
+	coracao_triplo_browser_height = document.documentElement.offsetHeight;
 }
 else
 {
-	snow_browser_width = 500;
-	snow_browser_height = 500;	
+	coracao_triplo_browser_width = 500;
+	coracao_triplo_browser_height = 500;	
 }
 
-snow_dx = [];
-snow_xp = [];
-snow_yp = [];
-snow_am = [];
-snow_stx = [];
-snow_sty = [];
+coracao_triplo_dx = [];
+coracao_triplo_xp = [];
+coracao_triplo_yp = [];
+coracao_triplo_am = [];
+coracao_triplo_stx = [];
+coracao_triplo_sty = [];
 
-for (i = 0; i < snow_no; i++) 
+for (i = 0; i < coracao_triplo_no; i++) 
 { 
-	snow_dx[i] = 0; 
-	snow_xp[i] = Math.random()*(snow_browser_width-50);
-	snow_yp[i] = Math.random()*snow_browser_height;
-	snow_am[i] = Math.random()*20; 
-	snow_stx[i] = 0.02 + Math.random()/10;
-	snow_sty[i] = 0.7 + Math.random();
-	if (i > 0) document.write("<\div id=\"snow_flake"+ i +"\" style=\"position:absolute;z-index:"+i+"\"><\img src=\""+snow_img+"\" border=\"0\"><\/div>"); else document.write("<\div id=\"snow_flake0\" style=\"position:absolute;z-index:0\"><a href=\"http://peters1.dk/tools/snow.php\" target=\"_blank\"><\img src=\""+snow_img+"\" border=\"0\"></a><\/div>");
+	coracao_triplo_dx[i] = 0; 
+	coracao_triplo_xp[i] = Math.random()*(coracao_triplo_browser_width-50);
+	coracao_triplo_yp[i] = Math.random()*coracao_triplo_browser_height;
+	coracao_triplo_am[i] = Math.random()*20; 
+	coracao_triplo_stx[i] = 0.02 + Math.random()/10;
+	coracao_triplo_sty[i] = 0.7 + Math.random();
+	if (i > 0) document.write("<\div id=\"coracao_triplo_flake"+ i +"\" style=\"position:absolute;z-index:"+i+"\"><\img src=\""+coracao_triplo_img+"\" border=\"0\"><\/div>"); else document.write("<\div id=\"coracao_triplo_flake0\" style=\"position:absolute;z-index:0\"><a href=\"http://peters1.dk/tools/coracao_triplo.php\" target=\"_blank\"><\img src=\""+coracao_triplo_img+"\" border=\"0\"></a><\/div>");
 }
 
-function SnowStart() 
+function Coracao_triploStart() 
 { 
-	for (i = 0; i < snow_no; i++) 
+	for (i = 0; i < coracao_triplo_no; i++) 
 	{ 
-		snow_yp[i] += snow_sty[i];
-		if (snow_yp[i] > snow_browser_height-50) 
+		coracao_triplo_yp[i] += coracao_triplo_sty[i];
+		if (coracao_triplo_yp[i] > coracao_triplo_browser_height-50) 
 		{
-			snow_xp[i] = Math.random()*(snow_browser_width-snow_am[i]-30);
-			snow_yp[i] = 0;
-			snow_stx[i] = 0.02 + Math.random()/10;
-			snow_sty[i] = 0.7 + Math.random();
+			coracao_triplo_xp[i] = Math.random()*(coracao_triplo_browser_width-coracao_triplo_am[i]-30);
+			coracao_triplo_yp[i] = 0;
+			coracao_triplo_stx[i] = 0.02 + Math.random()/10;
+			coracao_triplo_sty[i] = 0.7 + Math.random();
 		}
-		snow_dx[i] += snow_stx[i];
-		document.getElementById("snow_flake"+i).style.top=snow_yp[i]+"px";
-		document.getElementById("snow_flake"+i).style.left=snow_xp[i] + snow_am[i]*Math.sin(snow_dx[i])+"px";
+		coracao_triplo_dx[i] += coracao_triplo_stx[i];
+		document.getElementById("coracao_triplo_flake"+i).style.top=coracao_triplo_yp[i]+"px";
+		document.getElementById("coracao_triplo_flake"+i).style.left=coracao_triplo_xp[i] + coracao_triplo_am[i]*Math.sin(coracao_triplo_dx[i])+"px";
 	}
-	snow_time = setTimeout("SnowStart()", 10);
+	coracao_triplo_time = setTimeout("Coracao_triploStart()", 10);
 }
-SnowStart()
+Coracao_triploStart()
