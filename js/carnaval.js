4cf7aac9faecd6177faf143e7816d5b70c680d05
Rodrigo Rocha <2rptecnologia@gmail.com>
2019-01-20 18:28:55 -0200

    Update carnaval.js

--- js/carnaval.js
+++ js/carnaval.js
@@ -3,7 +3,10 @@
 //Exibe a queda de três ícones na tela e toca um samba enrredo ao mesmo tempo.
 //
 
-document.write("<h1>OLHA O CARNAVAL AÍ GENTE!!!!</h1>");
+document.write("<h1>OLHA O CARNAVAL GENTE!!!!</h1>");
+document.write("<h1>VAI FICAR PARADO? VAMOS CURTIR!!!!</h1>");
+document.write(" <img src="https://rodrigorochapinto.github.io/pages-teste/imagens/confete.png" alt="Smiley face" height="42" width="42"> ");
+
 
 
 // Caminho da Imagem
