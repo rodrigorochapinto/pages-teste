b4a2a883b2d44f4fed02301170282cf366443611
Rodrigo Rocha <2rptecnologia@gmail.com>
2019-01-20 07:23:39 -0200

    Update redirecionar.js

--- js/redirecionar.js
+++ js/redirecionar.js
@@ -1,3 +1,7 @@
-window.location.href = "https://rodrigorochapinto.github.io/pages-teste/paginas/fake_sigaa_COOP.html"
+//window.location.href = "https://rodrigorochapinto.github.io/pages-teste/paginas/fake_sigaa_COOP.html"
+
+function(usuario){
+	alert(usuario)
+}
 
 
