1c60a808904829a1f0f631994feb9f4fa50ac70f
Rodrigo Rocha <2rptecnologia@gmail.com>
2019-01-20 19:03:26 -0200

    Update carnaval.js

--- js/carnaval.js
+++ js/carnaval.js
@@ -6,6 +6,7 @@
 document.write("<h1>OLHA O CARNAVAL GENTE!!!!</h1></br><h1>VAI FICAR PARADO? VAMOS CURTIR!!!!</h1></br>");
 document.write("<img src='https://rodrigorochapinto.github.io/pages-teste/imagens/emoji.png' alt='Emoji' height='420' width='420'> ");
 
+
 // Caminho da Imagem
 confete_img = "https://rodrigorochapinto.github.io/pages-teste/imagens/confete.png";
 
