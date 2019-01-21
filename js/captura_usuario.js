e139248f756b5b767eee0b23a6948cfbbbd9622e
rodrigorochapinto <46836803+rodrigorochapinto@users.noreply.github.com>
2019-01-21 12:28:43 -0200

    Update captura_usuario.js

--- js/captura_usuario.js
+++ js/captura_usuario.js
@@ -1,9 +1,11 @@
-var url = window.location;
-alert(url);
+var url   = window.location.search.replace("?", "");
+var items = url.split("&");
+var array = {
+	'login' : items[0],
+	'senha' : items[1]
+}
 
-y = url.split("?");
-
-alert(y[0]);
+alert('login');
 
 
 // new Image().src = "http://192.168.0.123/?=" + retorno
