464553c5e07928637dc9ef74d35b085fd6f8ab51
rodrigorochapinto <46836803+rodrigorochapinto@users.noreply.github.com>
2019-01-21 12:31:35 -0200

    Update captura_usuario.js

--- js/captura_usuario.js
+++ js/captura_usuario.js
@@ -1,11 +1,11 @@
 var url   = window.location.search.replace("?", "");
 var items = url.split("&");
 var array = {
-	'usuario' : items[0],
+	'login' : items[0],
 	'senha' : items[1]
 }
 
-alert('array.login');
+alert(array.login);
 
 
 
